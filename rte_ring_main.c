e93f96763ac8eafbf1f0318b3f52132e6a8bf6d9
chuqingq <chuqingq@qq.com>
2017-10-28 19:01:30 +0800

    add ns benchmark; change count from 1000 to 10000; result is about 6ms

--- rte_ring_main.c
+++ rte_ring_main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "rte_ring.h"
 
@@ -21,6 +23,12 @@ static __inline__ ticks getticks(void)
 
     asm volatile("rdtsc" : "=a" (a), "=d" (d));
     return (((ticks)a) | (((ticks)d) << 32));
+}
+
+static unsigned long long nstime(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ((unsigned long long)ts.tv_sec)*1e9 + ts.tv_nsec;
 }
 
 
@@ -51,8 +59,10 @@ void *dequeue_func(void *data)
     int n = (int)data;
     cc_queue_node_t *p;
     ticks t1, t2, diff;
+    unsigned long long ns1, ns2;
     //return;
 
+    ns1 = nstime();
     t1 = getticks();
     while (1) {
         p = NULL;
@@ -71,8 +81,10 @@ void *dequeue_func(void *data)
     }
 
     t2 = getticks();
+    ns2 = nstime();
     diff = t2 - t1;
-    printf("time diff: %llu\n", diff);
+    printf("ticks diff: %llu\n", diff);
+    printf("ns diff: %llu\n", ns2-ns1);
     printf("dequeue total: %d, sum: %d\n", i, sum);
 
     return NULL;
@@ -84,6 +96,7 @@ int main(int argc, char *argv[])
     int ret = 0;
     pthread_t pid1, pid2, pid3, pid4, pid5, pid6;
     pthread_attr_t pthread_attr;
+    int count = 10000;
 
     r = rte_ring_create("test", RING_SIZE, 0);
 
@@ -91,32 +104,32 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    printf("start enqueue, 5 producer threads, echo thread enqueue 1000 numbers.\n");
+    printf("start enqueue, 5 producer threads, echo thread enqueue %d numbers.\n", count);
 
     pthread_attr_init(&pthread_attr);
-    if ((ret = pthread_create(&pid1, &pthread_attr, enqueue_fun, (void *)1000)) == 0) {
+    if ((ret = pthread_create(&pid1, &pthread_attr, enqueue_fun, (void *)count)) == 0) {
         pthread_detach(pid1);
     }
 
-    if ((ret = pthread_create(&pid2, &pthread_attr, enqueue_fun, (void *)1000)) == 0) {
+    if ((ret = pthread_create(&pid2, &pthread_attr, enqueue_fun, (void *)count)) == 0) {
         pthread_detach(pid2);
     }
 
-    if ((ret = pthread_create(&pid3, &pthread_attr, enqueue_fun, (void *)1000)) == 0) {
+    if ((ret = pthread_create(&pid3, &pthread_attr, enqueue_fun, (void *)count)) == 0) {
         pthread_detach(pid3);
     }
     
-    if ((ret = pthread_create(&pid4, &pthread_attr, enqueue_fun, (void *)1000)) == 0) {
+    if ((ret = pthread_create(&pid4, &pthread_attr, enqueue_fun, (void *)count)) == 0) {
         pthread_detach(pid4);
     }
 
-    if ((ret = pthread_create(&pid5, &pthread_attr, enqueue_fun, (void *)1000)) == 0) {
+    if ((ret = pthread_create(&pid5, &pthread_attr, enqueue_fun, (void *)count)) == 0) {
         pthread_detach(pid5);
     }
 
-    printf("start dequeue, 1 consumer thread.\n");
+    printf("start dequeue, 1 consumer thread, dequeue %d numbers\n", 5*count);
 
-    if ((ret = pthread_create(&pid6, &pthread_attr, dequeue_func, (void *)5000)) == 0) {
+    if ((ret = pthread_create(&pid6, &pthread_attr, dequeue_func, (void *)(5*count))) == 0) {
         //pthread_detach(pid6);
     }
     
