12aaa3614a000be67cee564e6ca48ae426626bab
chuqq <chuqingq@qq.com>
2017-10-30 10:04:07 +0800

    add 2p2c

--- rte_ring_main_2p2c.c
+++ rte_ring_main_2p2c.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
     pthread_t pid1, pid2, pid3, pid4, pid5, pid6, pid7, pid8;
     pthread_attr_t pthread_attr;
     int count = 100000000;
-    int per_count = count/4;
+    int per_count = count/2;
 
     r = rte_ring_create("test", RING_SIZE, 0);
 
@@ -77,23 +77,23 @@ int main(int argc, char *argv[])
     // producers
     pthread_create(&pid1, NULL, enqueue_fun, (void *)per_count);
     pthread_create(&pid2, NULL, enqueue_fun, (void *)per_count);
-    pthread_create(&pid3, NULL, enqueue_fun, (void *)per_count);
-    pthread_create(&pid4, NULL, enqueue_fun, (void *)per_count);
+    // pthread_create(&pid3, NULL, enqueue_fun, (void *)per_count);
+    // pthread_create(&pid4, NULL, enqueue_fun, (void *)per_count);
 
     // consumers
     pthread_create(&pid5, NULL, dequeue_func, (void *)per_count);
     pthread_create(&pid6, NULL, dequeue_func, (void *)per_count);
-    pthread_create(&pid7, NULL, dequeue_func, (void *)per_count);
-    pthread_create(&pid8, NULL, dequeue_func, (void *)per_count);
+    // pthread_create(&pid7, NULL, dequeue_func, (void *)per_count);
+    // pthread_create(&pid8, NULL, dequeue_func, (void *)per_count);
 
     pthread_join(pid1, NULL);
     pthread_join(pid2, NULL);
-    pthread_join(pid3, NULL);
-    pthread_join(pid4, NULL);
+    // pthread_join(pid3, NULL);
+    // pthread_join(pid4, NULL);
     pthread_join(pid5, NULL);
     pthread_join(pid6, NULL);
-    pthread_join(pid7, NULL);
-    pthread_join(pid8, NULL);
+    // pthread_join(pid7, NULL);
+    // pthread_join(pid8, NULL);
 
     t2 = nstime();
     printf("complete: count=%d, ns diff=%llu\n", count, t2-t1);
